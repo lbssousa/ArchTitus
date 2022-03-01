82b1156d729c57ccca894af5f55dc2ea84305ee4
Laércio de Sousa <lbsousajr@gmail.com>
2022-03-01 13:45:22 -0300

    Updated my personal package selection and initial settings.

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -33,19 +33,19 @@ fi
 # set kernel parameter for adding splash screen
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& splash /' /etc/default/grub
 
-echo -e "Installing CyberRe Grub theme..."
-THEME_DIR="/boot/grub/themes"
-THEME_NAME=CyberRe
-echo -e "Creating the theme directory..."
-mkdir -p "${THEME_DIR}/${THEME_NAME}"
-echo -e "Copying the theme..."
-cd ${HOME}/ArchTitus
-cp -a configs${THEME_DIR}/${THEME_NAME}/* ${THEME_DIR}/${THEME_NAME}
+#echo -e "Installing CyberRe Grub theme..."
+#THEME_DIR="/boot/grub/themes"
+#THEME_NAME=CyberRe
+#echo -e "Creating the theme directory..."
+#mkdir -p "${THEME_DIR}/${THEME_NAME}"
+#echo -e "Copying the theme..."
+#cd ${HOME}/ArchTitus
+#cp -a configs${THEME_DIR}/${THEME_NAME}/* ${THEME_DIR}/${THEME_NAME}
 echo -e "Backing up Grub config..."
 cp -an /etc/default/grub /etc/default/grub.bak
-echo -e "Setting the theme as the default..."
-grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sed -i '/GRUB_THEME=/d' /etc/default/grub
-echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
+#echo -e "Setting the theme as the default..."
+#grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sed -i '/GRUB_THEME=/d' /etc/default/grub
+#echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
 echo -e "Updating grub..."
 grub-mkconfig -o /boot/grub/grub.cfg
 echo -e "All set!"
@@ -126,18 +126,33 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 PLYMOUTH_THEMES_DIR="$HOME/ArchTitus/configs/usr/share/plymouth/themes"
-PLYMOUTH_THEME="arch-glow" # can grab from config later if we allow selection
+PLYMOUTH_THEME="bgrt" # can grab from config later if we allow selection
 mkdir -p /usr/share/plymouth/themes
-echo 'Installing Plymouth theme...'
-cp -rf ${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME} /usr/share/plymouth/themes
+#echo 'Installing Plymouth theme...'
+#cp -rf ${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME} /usr/share/plymouth/themes
 if  [[ $FS == "luks"]]; then
   sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
   sed -i 's/HOOKS=(base udev \(.*block\) /&plymouth-/' /etc/mkinitcpio.conf # create plymouth-encrypt after block hook
 else
   sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
 fi
-plymouth-set-default-theme -R arch-glow # sets the theme and runs mkinitcpio
-echo 'Plymouth theme installed'
+plymouth-set-default-theme -R bgrt # sets the theme and runs mkinitcpio
+#echo 'Plymouth theme installed'
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Enabling Flathub repository
+-------------------------------------------------------------------------
+"
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Configuring snapd
+-------------------------------------------------------------------------
+"
+ln -s /var/lib/snapd/snap /snap
+systemctl enable snapd.socket
 
 echo -ne "
 -------------------------------------------------------------------------
