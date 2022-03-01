475799226d6859a10fe16373253cb5ca210c6a4b
Laércio de Sousa <lbsousajr@gmail.com>
2022-03-01 13:51:58 -0300

    Adding flicker-free boot parameters.

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -31,7 +31,7 @@ if [[ "${FS}" == "luks" ]]; then
 sed -i "s%GRUB_CMDLINE_LINUX_DEFAULT=\"%GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=UUID=${ENCRYPTED_PARTITION_UUID}:ROOT root=/dev/mapper/ROOT %g" /etc/default/grub
 fi
 # set kernel parameter for adding splash screen
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& splash /' /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& quiet splash i915.fastboot=1 /' /etc/default/grub
 
 #echo -e "Installing CyberRe Grub theme..."
 #THEME_DIR="/boot/grub/themes"
